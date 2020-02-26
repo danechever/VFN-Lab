8dfd1856d4297682d27b32dae28c37381ebb1a88
Dan <dechever@caltech.edu>
2020-02-26 09:45:42 -0800

    Modify PM100D to possibly work with Linux
    Modified VFN_setUpPM100D to recognize OS and connect via NI-Visa for Windows or USBTMC for linux. This should make the whole library Linux compatible.
    
    The library is still untested on Linux but it works on Windows.

--- ControlCode/PowerMeterCode/VFN_setUpPM100D.m
+++ ControlCode/PowerMeterCode/VFN_setUpPM100D.m
@@ -4,6 +4,8 @@
 %- Attempts to automatically connect to first PM100D found by system
 %- Commented-out section allows manual entry of PM100D info
 %
+%* Attempts ot connect automatically on Windows or Linux
+%
 %This uses the ThorlabsPM100 python library
 %- To install: https://github.com/clade/ThorlabsPM100
 %- Relies on PyVisa: https://pyvisa.readthedocs.io/en/stable/
@@ -14,39 +16,51 @@
 %  author: D. Echeverri (dechever@caltech.edu)
 %  created: 02/25/2020
 
-% Define manufacturer ID and product ID for auto-device detect
-PM_IDS = '0x1313::0x8078';
+if ispc
+    %-- Windows PC detected. Act accordingly
+    % Define manufacturer ID and product ID for auto-device detect
+    PM_IDS = '0x1313::0x8078';
 
-% Instantiate PyVisa object
-PM.rm = py.visa.ResourceManager();
+    % Instantiate PyVisa object
+    PM.rm = py.visa.ResourceManager();
 
-%-- Connect to power meter (find first PM100D device automatically)
-% List all devices visible to PyVisa
-PM.devs = PM.rm.list_resources();
-% Iterate through list looking for first PM100D device
-for devs_i = 1:py.len(PM.devs)
-	if strfind(char(PM.devs{devs_i}), PM_IDS)
-        % Device with matching Manufacturer and Product ID was found
-        PM.dev = char(PM.devs{devs_i});
-        break
+    %-- Connect to power meter (find first PM100D device automatically)
+    % List all devices visible to PyVisa
+    PM.devs = PM.rm.list_resources();
+    % Iterate through list looking for first PM100D device
+    for devs_i = 1:py.len(PM.devs)
+        if strfind(char(PM.devs{devs_i}), PM_IDS)
+            % Device with matching Manufacturer and Product ID was found
+            PM.dev = char(PM.devs{devs_i});
+            break
+        end
+        if devs_i == py.len(PM.devs)
+            % Looked at all devices; none matched our ID string
+            error('No PM100D was auto-detected. Try manually connecting (see setUp code)')
+        end
     end
-    if devs_i == py.len(PM.devs)
-        % Looked at all devices; none matched our ID string
-        error('No PM100D was auto-detected. Try manually connecting (see setUp code)')
-    end
+    % Instantiate instrument object and connect to device
+    PM.inst = PM.rm.open_resource(PM.dev, pyargs('timeout',1));
+
+    % %-- Manual way to connect to PM100D
+    % %- Set the Serial Number (P0022602 in this exmple) to match your device
+    % try
+    %     PM.inst = PM.rm.open_resource('USB0::0x1313::0x8078::P0022602::0::INSTR',pyargs('timeout',1));
+    % catch pyERR
+    %     disp(getReport(pyERR,'basic'))
+    %     error('Error connecting to device. Check serial number and that device is connected')
+    % end
+elseif isunix
+    %-- Linux Machine detected. Act accordingly
+    % Connect using USBTMC to default device (/dev/usbtmc0)
+    PM.inst = py.ThorlabsPM100.USBTMC();
+    
+	% %-- If default USBTMC path fails, provide path here
+	% PM.inst = py.ThorlabsPM100.USBTMC(pyargs('device','/dev/usbtmc0'));
+    
+    % Alias PM.rm to PM.inst so that cleanUp func. will work correctly
+    PM.rm = PM.inst;
 end
-% Instantiate instrument object and connect to device
-PM.inst = PM.rm.open_resource(PM.dev, pyargs('timeout',1));
-
-% %-- Manual way to connect to PM100D
-% %- Set the Serial Number (P0022602 in this exmple) to match your device
-% try
-%     PM.inst = PM.rm.open_resource('USB0::0x1313::0x8078::P0022602::0::INSTR',pyargs('timeout',1));
-% catch pyERR
-%     disp(getReport(pyERR,'basic'))
-%     error('Error connecting to device. Check serial number and that device is connected')
-% end
-
 
 % Instantiate device main comms object
 PM.PM = py.ThorlabsPM100.ThorlabsPM100(PM.inst);
